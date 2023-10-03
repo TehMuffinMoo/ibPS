6d4b67b08a57363ee8fb6e667c97ae990b5a0cd1
TehMuffinMoo <mat.cox@tmmn.uk>
2023-10-03 09:00:34 +0100

    add b1 threat feeds

---
+++ Modules/ibPS/Functions/BloxOneDDI/Get-B1ThreatFeeds.ps1
@@ -0,0 +1,50 @@
+function Get-B1ThreatFeeds {
+    <#
+    .SYNOPSIS
+        Use this cmdlet to retrieve information on all Threat Feed objects for the account
+
+    .DESCRIPTION
+        Use this cmdlet to retrieve information on all Threat Feed objects for the account. BloxOne Cloud provides predefined threat intelligence feeds based on your subscription. The Plus subscription offers a few more feeds than the Standard subscription. The Advanced subscription offers a few more feeds than the Plus subscription. A threat feed subscription for RPZ updates offers protection against malicious hostnames.
+
+    .PARAMETER Name
+        Use this parameter to filter the list of Subnets by Name
+
+    .PARAMETER Strict
+        Use strict filter matching. By default, filters are searched using wildcards where possible. Using strict matching will only return results matching exactly what is entered in the applicable parameters.
+
+    .Example
+        Get-B1ThreatFeeds -Name "FarSightNOD","AntiMalware"
+    
+    .FUNCTIONALITY
+        BloxOneDDI
+    
+    .FUNCTIONALITY
+        BloxOne Threat Defense
+    #>
+    param(
+        [String]$Name,
+        [Switch]$Strict
+    )
+ 
+	$MatchType = Match-Type $Strict
+
+    [System.Collections.ArrayList]$Filters = @()
+
+    if ($Name) {
+        $Filters.Add("name$($MatchType)`"$Name`"") | Out-Null
+    }
+
+    if ($Filters) {
+        $Filter = Combine-Filters $Filters
+    }
+
+    if ($Filter) {
+      $Results = Query-CSP -Uri "$(Get-B1CspUrl)/api/atcfw/v1/threat_feeds?_filter=$Filter" -Method GET | select -ExpandProperty results -ErrorAction SilentlyContinue
+    } else {
+      $Results = Query-CSP -Uri "$(Get-B1CspUrl)/api/atcfw/v1/threat_feeds" -Method GET | select -ExpandProperty results -ErrorAction SilentlyContinue
+    }
+  
+    if ($Results) {
+      return $Results
+    }
+}
