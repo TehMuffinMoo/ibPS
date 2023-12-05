4c18c3c8c79fdf68166cc179a2d6b1542b3fd30c
TehMuffinMoo <mat.cox@tmmn.uk>
2023-12-05 12:24:13 +0000

    Add -Update param to Get-ibPSVersion to enable in-place upgrades of the module

--- Modules/BloxOne-Main.ps1
+++ Modules/BloxOne-Main.ps1
@@ -57,16 +57,55 @@ function DeprecationNotice {
 
 function Get-ibPSVersion {
   param (
-    [Switch]$CheckForUpdates
+    [Switch]$CheckForUpdates,
+    [Switch]$Update
   )
   $CurrentVersion = (Get-Module -ListAvailable -Name ibPS).Version.ToString()
-  if ($CheckForUpdates) {
+  if ($CheckForUpdates -or $Update) {
     $ModuleManifest = Invoke-WebRequest -Uri "https://raw.githubusercontent.com/TehMuffinMoo/ibPS/main/Modules/ibPS/ibPS.psd1"
-    $LatestVersion = ($ModuleManifest.RawContent | Select-String -Pattern 'ModuleVersion\s\=\s(.*)').Matches.Groups[1].Value -replace "'",""
-    if ($LatestVersion -gt $CurrentVersion) {
-      Write-Host "New version available! Current Version: $CurrentVersion - Latest Version: $LatestVersion" -BackgroundColor Yellow -ForegroundColor DarkGreen
+    if ($ModuleManifest) {
+      $LatestVersion = ($ModuleManifest.RawContent | Select-String -Pattern 'ModuleVersion\s\=\s(.*)').Matches.Groups[1].Value -replace "'",""
+      if ($LatestVersion -gt $CurrentVersion) {
+        Write-Host "New version available! Current Version: $CurrentVersion - Latest Version: $LatestVersion" -BackgroundColor Yellow -ForegroundColor DarkGreen
+        if ($Update) {
+          Write-Warning "Confirmation: Do you want to proceed with updating from v$CurrentVersion to v$LatestVersion?" -WarningAction Inquire
+          Invoke-WebRequest -Uri "https://github.com/TehMuffinMoo/ibPS/archive/refs/heads/main.zip" -OutFile ibPS.zip
+          if (Test-Path ibPS.zip) {
+            Expand-Archive ibPS.zip
+          }
+          if (Test-Path ibPS) {
+            $ModulePath = (Get-Module -ListAvailable -Name ibPS).Path
+            $Platform = Detect-OS
+            if ($Platform -eq "Windows") {
+              if ($ModulePath -like "$ENV:USERPROFILE\*") {
+                $Selection = "i"
+              } elseif ($ModulePath -like "C:\Windows\System32\WindowsPowerShell\*") {
+                $Selection = "g"
+              }
+            }
+            if ($Platform -eq "Mac" -or $Platform -eq "Unix") {
+              if ($ModulePath -like "$($ENV:HOME)/*") {
+                $Selection = "i"
+              } elseif ($ModulePath -like "/usr/local/*") {
+                $Selection = "g"
+              }
+            }
+            cd ibPS/ibPS-main
+            .\Install.ps1 -Selection $Selection
+          }
+          cd ../../
+          Remove-Item ibPS,ibPS.zip -Recurse -Force
+          if ($CurrentVersion = (Get-Module -ListAvailable -Name ibPS).Version.ToString() -eq $LatestVersion) {
+            Write-Host "Successfully updated ibPS to latest version: $LatestVersion" -ForegroundColor Green
+          } else {
+            Write-Error "Failed to update ibPS to latest version. Current Version: $CurrentVersion"
+          }
+        }
+      } else {
+        Write-Host "ibPS is up to date! Current Version: $CurrentVersion - Latest Version: $LatestVersion" -BackgroundColor Green -ForegroundColor Blue
+      }
     } else {
-      Write-Host "ibPS is up to date! Current Version: $CurrentVersion - Latest Version: $LatestVersion" -BackgroundColor Green -ForegroundColor Blue
+      Write-Error "Unable to retrieve latest version information from Github."
     }
   } else {
     return $CurrentVersion
