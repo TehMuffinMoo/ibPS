ae8aba64aba45cd8cdfe7f22db0dbcf49fe616b3
TehMuffinMoo <mat.cox@tmmn.uk>
2024-03-07 11:46:02 +0000

    Update docs

--- Modules/ibPS/Functions/BloxOneDDI/Get-B1SecurityLog.ps1
+++ Modules/ibPS/Functions/BloxOneDDI/Get-B1SecurityLog.ps1
@@ -103,7 +103,6 @@ function Get-B1SecurityLog {
     if ($QueryFilters) {
         $QueryString = ConvertTo-QueryString $QueryFilters
     }
-    $QueryString
     if ($QueryString) {
         $Results = Query-CSP -Uri "$(Get-B1CSPUrl)/security-events/v1/security_events$($QueryString)" -Method GET | Select-Object -ExpandProperty results -ErrorAction SilentlyContinue
     } else {
