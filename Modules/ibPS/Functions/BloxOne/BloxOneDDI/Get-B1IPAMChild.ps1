6213b93564b53d98d0477960fbd82f46bbe0c2ae
TehMuffinMoo <mat.cox@tmmn.uk>
2024-03-31 13:37:37 +0100

    improvements

--- Modules/ibPS/Functions/BloxOne/BloxOneDDI/Get-B1IPAMChild.ps1
+++ Modules/ibPS/Functions/BloxOne/BloxOneDDI/Get-B1IPAMChild.ps1
@@ -8,8 +8,17 @@
 
         This accepts pipeline input from Get-B1Space, Get-B1AddressBlock, Get-B1Subnet & Get-B1Range
 
-    .PARAMETER id
-        The ID of the parent object to list child objects for
+    .PARAMETER Type
+        Filter results by the object type
+
+    .PARAMETER Label
+        Filter results by the object label
+
+    .PARAMETER Description
+        Filter results by the object description
+
+    .PARAMETER Object
+        The parent object to list child objects for
 
     .PARAMETER Limit
         Limits the number of results returned, the default is 100
@@ -20,20 +29,26 @@
     .PARAMETER Fields
         Specify a list of fields to return. The default is to return all fields.
 
+    .PARAMETER Strict
+        Use strict filter matching. By default, filters are searched using wildcards where possible. Using strict matching will only return results matching exactly what is entered in the applicable parameters.
+
     .PARAMETER OrderBy
         Optionally return the list ordered by a particular value. If sorting is allowed on non-flat hierarchical resources, the service should implement a qualified naming scheme such as dot-qualification to reference data down the hierarchy. Using 'asc' or 'desc' as a suffix will change the ordering, with ascending as default.
 
     .PARAMETER OrderByTag
         Optionally return the list ordered by a particular tag value. Using 'asc' or 'desc' as a suffix will change the ordering, with ascending as default.
 
+    .PARAMETER tfilter
+        Use this parameter to filter the results returned by tag.
+
     .EXAMPLE
         PS> Get-B1Space -Name "my-ipspace" | Get-B1IPAMChild
 
     .EXAMPLE
-        PS> Get-B1AddressBlock -Subnet "10.10.10.0" -CIDR 24 -Space "my-ipspace" | Get-B1IPAMChild
+        PS> Get-B1AddressBlock -Subnet "10.10.10.0" -CIDR 24 -Space "my-ipspace" | Get-B1IPAMChild -Type 'ipam/subnet'
         
     .EXAMPLE
-        PS> Get-B1Subnet -Subnet "10.10.10.0" -CIDR 24 -Space "my-ipspace" | Get-B1IPAMChild
+        PS> Get-B1Subnet -Subnet "10.10.10.0" -CIDR 24 -Space "my-ipspace" | Get-B1IPAMChild -Type 'ipam/subnet'
 
     .FUNCTIONALITY
         BloxOneDDI
@@ -42,34 +57,54 @@
         DHCP
     #>
     param (
-        [Parameter(
-            ValueFromPipelineByPropertyName = $true,
-            Mandatory=$true
-        )]
-        [String[]]$ID,
+        [String]$Type,
+        [String]$Label,
+        [String]$Description,
         [Int]$Limit = 100,
         [Int]$Offset = 0,
+        [Switch]$Strict = $false,
         [String[]]$Fields,
         [String]$OrderBy,
-        [String]$OrderByTag
+        [String]$OrderByTag,
+        [String]$tfilter,
+        [Parameter(
+            ValueFromPipeline = $true,
+            Mandatory=$true
+        )]
+        [System.Object[]]$Object
     )
 
     process {
+        $MatchType = Match-Type $Strict
         $PermittedInputs = "ip_space","address_block","subnet","range"
-        if (($ID.split('/')[1]) -notin $PermittedInputs) {
+        if (($Object.id.split('/')[1]) -notin $PermittedInputs) {
             Write-Error "Error. Unsupported pipeline object. Supported inputs are ip_space, address_block, subnet & range"
             return $null
         }
 
-        if ($Limit) {
-        $LimitString = "_limit=$($Limit)&"
-        }
-
+        [System.Collections.ArrayList]$Filters = @()
         [System.Collections.ArrayList]$QueryFilters = @()
-        $QueryFilters.Add("node=$ID") | Out-Null
+        if ($Type) {
+            $Filters.Add("type$($MatchType)`"$($Type)`"") | Out-Null
+        }
+        if ($Label) {
+            $Filters.Add("label$($MatchType)`"$($Label)`"") | Out-Null
+        }
+        if ($Description) {
+            $Filters.Add("description$($MatchType)`"$($Description)`"") | Out-Null
+        }
+        if ($Filters) {
+            $Filter = Combine-Filters $Filters
+            $QueryFilters.Add("_filter=$Filter") | Out-Null
+        }
+        $QueryFilters.Add("node=$($Object.id)") | Out-Null
         $QueryFilters.Add("view=SPACE") | Out-Null
-        $QueryFilters.Add("_limit=$Limit") | Out-Null
-        $QueryFilters.Add("_offset=$Offset") | Out-Null
+        if ($Limit) {
+            $QueryFilters.Add("_limit=$Limit") | Out-Null
+        }
+        if ($Offset) {
+            $QueryFilters.Add("_offset=$Offset") | Out-Null
+        }
         if ($Fields) {
             $QueryFilters.Add("_fields=$($Fields -join ",")") | Out-Null
         }
@@ -78,6 +113,9 @@
         }
         if ($OrderByTag) {
             $QueryFilters.Add("_torder_by=$OrderByTag") | Out-Null
+        }
+        if ($tfilter) {
+            $QueryFilters.Add("_tfilter=$tfilter") | Out-Null
         }
         if ($QueryFilters) {
             $QueryString = ConvertTo-QueryString $QueryFilters
