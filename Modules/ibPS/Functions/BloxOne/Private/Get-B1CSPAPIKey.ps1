4dfcf5eeb34d6faae3d795db8a7bc38012ef765b
TehMuffinMoo <mat.cox@tmmn.uk>
2024-04-24 18:47:12 +0100

    Update message

--- Modules/ibPS/Functions/BloxOne/Private/Get-B1CSPAPIKey.ps1
+++ Modules/ibPS/Functions/BloxOne/Private/Get-B1CSPAPIKey.ps1
@@ -31,7 +31,7 @@ function Get-B1CSPAPIKey {
             }
         } catch {
             Write-Colour 'Error. Unable to decode the API Key. Please set the API Key again using: ','Set-ibPSConfiguration -CSPAPIKey <apikey>' -Colour Red,Green
-            Write-Colour 'If you have recently upgraded from a version older than ','v1.9.4.4',', you will need to update your API Key.' -Colour Yellow,Red,Yellow
+            Write-Colour 'If you have recently upgraded from a version older than ','v1.9.5.0',', you will need to update your API Key.' -Colour Yellow,Red,Yellow
             Write-Colour "See the following link for more information: ","https://ibps.readthedocs.io/en/latest/General/Set-ibPSConfiguration/" -Colour Gray,Magenta
             break
         }
