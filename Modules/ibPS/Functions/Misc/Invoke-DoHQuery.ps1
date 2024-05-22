a10d279a28d8995d22c1b43317e28d9531c2b3d3
TehMuffinMoo <mat.cox@tmmn.uk>
2024-05-22 16:52:35 +0100

    Update docs

--- Modules/ibPS/Functions/Misc/Invoke-DoHQuery.ps1
+++ Modules/ibPS/Functions/Misc/Invoke-DoHQuery.ps1
@@ -18,21 +18,51 @@ function Invoke-DoHQuery {
         Optionally specify the DNS request type
 
     .EXAMPLE
-        PS> Invoke-DoHQuery -Query google.com      
+        PS> Invoke-DoHQuery -Query google.com -Type TXT
                                                                                                                         
-        QNAME      QTYPE QCLASS Answers
-        -----      ----- ------ -------
-        google.com       IN     {@{RDATA=System.Collections.Hashtable; RNAME=google.com; RTYPE=SOA; RCLASS=IN; TTL=60; LENGTH=58}}
+        QNAME         : google.com
+        QTYPE         : TXT
+        QCLASS        : IN
+        AnswerRRs     : {@{RDATA=docusign=05958488-4752-4ef2-95eb-aa7ba8a3bd0e; RNAME=google.com; RTYPE=TXT; RCLASS=IN; TTL=3600; LENGTH=46; TXT_LENGTH=45}, @{RDATA=v=spf1 include:_spf.google.com ~all; RNAME=google.com; RTYPE=TXT; RCLASS=IN; TTL=3600; LENGTH=36; TXT_LENGTH=35}, 
+                        @{RDATA=google-site-verification=TV9-DBe4R80X4v0M4U_bd_J9cpOJM0nikft0jAgjmsQ; RNAME=google.com; RTYPE=TXT; RCLASS=IN; TTL=3600; LENGTH=69; TXT_LENGTH=68}, @{RDATA=globalsign-smime-dv=CDYX+XFHUw2wml6/Gb8+59BsH31KzUr6c1l2BPvqKX8=; RNAME=google.com; RTYPE=TXT; RCLASS=IN; TTL=3600; 
+                        LENGTH=65; TXT_LENGTH=64}…}
+        AuthorityRRs  : {}
+        AdditionalRRs : {}
+        Headers       : {[AnswerRRs, 11], [AdditionalRRs, 0], [Questions, 1], [TransactionID, 0]…}
 
     .EXAMPLE
-        PS> Invoke-DoHQuery -Query bbc.co.uk -Type A | Select-Object -ExpandProperty Answers | ft -AutoSize
-                                                                                                                            
+        PS> Invoke-DoHQuery -Query google.com -Type TXT | Select-Object -ExpandProperty AnswerRRs | ft -AutoSize
+                                                                                                                                
+        RDATA                                                                RNAME      RTYPE RCLASS  TTL LENGTH TXT_LENGTH
+        -----                                                                -----      ----- ------  --- ------ ----------
+        google-site-verification=TV9-DBe4R80X4v0M4U_bd_J9cpOJM0nikft0jAgjmsQ google.com TXT   IN     3600     69         68
+        docusign=1b0a6754-49b1-4db5-8540-d2c12664b289                        google.com TXT   IN     3600     46         45
+        facebook-domain-verification=22rm551cu4k0ab0bxsw536tlds4h95          google.com TXT   IN     3600     60         59
+        globalsign-smime-dv=CDYX+XFHUw2wml6/Gb8+59BsH31KzUr6c1l2BPvqKX8=     google.com TXT   IN     3600     65         64
+        webexdomainverification.8YX6G=6e6922db-e3e6-4a36-904e-a805c28087fa   google.com TXT   IN     3600     67         66
+        apple-domain-verification=30afIBcvSuDV2PLX                           google.com TXT   IN     3600     43         42
+        v=spf1 include:_spf.google.com ~all                                  google.com TXT   IN     3600     36         35
+        docusign=05958488-4752-4ef2-95eb-aa7ba8a3bd0e                        google.com TXT   IN     3600     46         45
+        google-site-verification=wD8N7i1JTNTkezJ49swvWW48f8_9xveREV4oB-0Hf5o google.com TXT   IN     3600     69         68
+        MS=E4A68B9AB2BB9670BCE15412F62916164C0B20BB                          google.com TXT   IN     3600     44         43
+        onetrust-domain-verification=de01ed21f2fa4d8781cbc3ffb89cf4ef        google.com TXT   IN     3600     62         61
+
+    .EXAMPLE
+        PS> Invoke-DoHQuery -Query bbc.co.uk -Type SOA | Select-Object -ExpandProperty AnswerRRs | Select-Object -ExpandProperty RDATA | ft -AutoSize
+                                                                                                                        
+        NS           ADMIN                    SERIAL REFRESH RETRY EXPIRE TTL
+        --           -----                    ------ ------- ----- ------ ---
+        ns.bbc.co.uk hostmaster.bbc.co.uk 2024052100    1800   600 864000 900
+
+    .EXAMPLE
+        PS> Invoke-DoHQuery -Query bbc.co.uk -Type A | Select-Object -ExpandProperty AnswerRRs | ft -AutoSize
+                                                                                                                        
         RDATA          RNAME     RTYPE RCLASS TTL LENGTH
         -----          -----     ----- ------ --- ------
-        151.101.128.81 bbc.co.uk A     IN     296      4
-        151.101.192.81 bbc.co.uk A     IN     296      4
-        151.101.0.81   bbc.co.uk A     IN     296      4
-        151.101.64.81  bbc.co.uk A     IN     296      4
+        151.101.192.81 bbc.co.uk A     IN     163      4
+        151.101.0.81   bbc.co.uk A     IN     163      4
+        151.101.64.81  bbc.co.uk A     IN     163      4
+        151.101.128.81 bbc.co.uk A     IN     163      4
     #>
     param(
         [String]$Query,
@@ -99,6 +129,7 @@ function Invoke-DoHQuery {
         'MX' = 15
         'TXT' = 16
         'AAAA' = 28
+        'ANY' = 255
     }
 
     $QCLASSList = @{
