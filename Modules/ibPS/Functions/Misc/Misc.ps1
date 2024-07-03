d17dd7fda8272f7df4ac43cd952915476d601196
TehMuffinMoo <mat.cox@tmmn.uk>
2024-07-03 10:47:43 +0100

    win test

--- Modules/ibPS/Functions/Misc/Misc.ps1
+++ Modules/ibPS/Functions/Misc/Misc.ps1
@@ -838,8 +838,8 @@ function Build-HTMLTopologyChildren {
 function ConvertFrom-ComplexJSON([string]$text) {                           
   $parser = New-Object Web.Script.Serialization.JavaScriptSerializer
   $parser.MaxJsonLength = $text.length
-  #Write-Output -NoEnumerate $parser.Deserialize($text, [hashtable])
-  Write-Output -NoEnumerate $parser.DeserializeObject($text)
+  Write-Output -NoEnumerate (($parser.Deserialize($text, [hashtable])) | ConvertFrom-HashTable)
+  #Write-Output -NoEnumerate $parser.DeserializeObject($text)
   # To deserialize to a dictionary, use $parser.DeserializeObject($text) instead
 }
 
